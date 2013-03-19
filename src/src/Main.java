1d862c6e06f091e3c47029c4a0dd3088e5dab1d1
Jonatan Magnusson <kjb.magnusson@gmail.com>
2013-03-19 12:33:56 +0100

    testJonatan
    4ö4ö4ö4ö4ö4ö4ö4ö4ö4ö4ö4ö

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("testLinus");
 		System.out.println("testCalleberg");
+		System.out.println("testJonatan");
 	}
 }
