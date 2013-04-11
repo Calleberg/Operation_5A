963911e2341c6603f3bcd114eca73f5bb1d64e46
Linus Hagvall <hagvall@student.chalmers.se>
2013-04-11 20:22:57 +0200

    linus

--- src/vidar/Main.java
+++ src/vidar/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Best main is my main");
+		System.out.println("Best main is my msain");
 //Linus
 		//West coast is best coast
 	}
