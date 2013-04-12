1468681f47e02d3616cab58ed1d8c8e57380aaca
Martin Calleberg <martin.calleberg@gmail.com>
2013-04-12 10:12:34 +0200

    Diverse
    world, gamemodel

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,10 +1,16 @@
 package model;
 
+import sprite.Player;
+import world.World;
+
 public class GameModel {
+	
 	private final World world;
 	private final Player player;
+	
 	public GameModel(){
 		world = new World();
+		player = new Player(1,1);
 		world.addSprite(player);
 		
 	}
--- src/world/World.java
+++ src/world/World.java
@@ -1,14 +1,19 @@
 package world;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import sprite.Player;
+
 public class World {
 
-//	private List<Player> sprites = new ArrayList<Player>();
-//
-//	public void addSprite(Player sprite) {
-//		this.sprites.add(sprite);
-//	}
-//	
-//	public List<Player> getSprites() {
-//		return this.sprites;
-//	}
+	private List<Player> sprites = new ArrayList<Player>();
+
+	public void addSprite(Player sprite) {
+		this.sprites.add(sprite);
+	}
+	
+	public List<Player> getSprites() {
+		return this.sprites;
+	}
 }
