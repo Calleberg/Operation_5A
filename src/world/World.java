dc430ca65f7f744aaa5c6f7fdb101ed16301df20
Martin Calleberg <martin.calleberg@gmail.com>
2013-04-12 10:06:59 +0200

    World added

---
+++ src/world/World.java
@@ -0,0 +1,14 @@
+package world;
+
+public class World {
+
+//	private List<Player> sprites = new ArrayList<Player>();
+//
+//	public void addSprite(Player sprite) {
+//		this.sprites.add(sprite);
+//	}
+//	
+//	public List<Player> getSprites() {
+//		return this.sprites;
+//	}
+}
