c4b117c2aa62f57c6cea2270fe889fa29eb13593
Jonatan Magnusson <kjb.magnusson@gmail.com>
2013-04-15 16:55:51 +0200

    Projectile, Weapon
    added projectile and updated weapon + javadoc

---
+++ src/projectile/Projectile.java
@@ -0,0 +1,61 @@
+package projectile;
+
+import Item.Weapon;
+import sprite.Player;
+import geometrical.Position;
+
+public class Projectile {
+	
+	private int damage;
+	private float speed;
+	private float range;
+	private float direction;
+	private Position position;
+	
+	public Projectile(Player player, Weapon weapon){
+		this.damage = weapon.getDamage();
+		this.speed = weapon.getSpeed();
+		this.range = weapon.getRange();
+		this.direction = player.getDirection();
+		this.position = new Position(player.getX(), player.getY());
+	}
+	/**
+	 * Returns the amount of damage the projectile causes
+	 * @return the damage of the projectile
+	 */
+	public int getDamage(){
+		return damage;
+	}
+	/**
+	 * Returns the speed of the projectile
+	 * @return the speed of the projectile
+	 */
+	public float getSpeed(){
+		return speed;
+	}
+	/**
+	 * Returns the range the projectile can travel, the range will be the same for 
+	 * all ranged weapons but differ for melee weapons
+	 * @return the range of the projectile
+	 */
+	public float getRange(){
+		return range;
+	}
+	/**
+	 * Returns the direction in which the projectile will travel, 
+	 * the value is in radians
+	 * @return the direction of the projectile
+	 */
+	public float getDirection(){
+		return direction;
+	}
+	
+	/**
+	 * Returns the position of the projectile
+	 * @return the position of the projectile
+	 */
+	public Position getPosition(){
+		return position;
+	}
+
+}
