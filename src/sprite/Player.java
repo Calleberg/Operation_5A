3c8a608f0796a4c0dfe27e0ed590eb47d6b8c977
Linus Hagvall <hagvall@student.chalmers.se>
2013-04-15 16:50:56 +0200

    javadoc player, gamecontroller

--- src/sprite/Player.java
+++ src/sprite/Player.java
@@ -3,19 +3,29 @@ package sprite;
 import geometrical.Position;
 
 public class Player implements Sprite {
-	
+	/**
+	 * The player's current state. E.g. forward or backward.
+	 */
 	public static enum State{FORWARD, BACKWARDS, RIGHT, LEFT, STANDING};
 	private State state;
 	private Position position;
 	private float direction;
 	private float speed;
 	
+	/**
+	 * Creates a new player with a specific position.
+	 * @param x The x-coordinate for the player's position.
+	 * @param y The y-coordinate for the player's position.
+	 */
 	public Player(float x, float y){
 		state = State.STANDING;
 		position = new Position(x,y);
 		this.speed = 0.2f;
 	}
-	
+	/**
+	 * The player moves in specific direction and length depending on which key is pressed
+	 * and the player's speed.
+	 */
 	public void move(){
 		if(state == State.FORWARD) {
 			changePosition(direction);
@@ -27,30 +37,52 @@ public class Player implements Sprite {
 			changePosition(direction-Math.PI/2);
 		}
 	}
-	
+	/*
+	 * Changes the position with a specific direction.
+	 */
 	private void changePosition(double d){
 		position.setX(position.getX() + (float)(Math.cos(d)*speed));
 		position.setY(position.getY() - (float)(Math.sin(d)*speed));
 	}
-	
+	/**
+	 * Set the state for the player.
+	 */
 	public void setState(State state) {
 		this.state = state;
 	}
+	/**
+	 * Returns the direction of the player.
+	 */
 	public float getDirection(){
 		return direction;
 	}
+	/**
+	 * Set the direction of the player.
+	 */
 	public void setDirection(float direction){
 		this.direction = direction;
 	}
+	/**
+	 * Returns the x-coordinate of the player's position.
+	 */
 	public float getX(){
 		return position.getX();
 	}
+	/**
+	 * Returns the y-coordinate of the player's position.
+	 */
 	public float getY(){
 		return position.getY();
 	}
+	/**
+	 * Set the x-coordinate of the player's position.
+	 */
 	public void setX(float x){
 		position.setX(x);
 	}
+	/**
+	 * Set the y-coordinate of the player's position.
+	 */
 	public void setY(float y){
 		position.setY(y);
 	}
