173549441c7ac2c1ffea39ff133c92bfa9d97f4a
Martin Calleberg <martin.calleberg@gmail.com>
2013-04-15 15:32:19 +0200

    Test merge

--- src/base/Main.java
+++ src/base/Main.java
@@ -9,12 +9,15 @@ public class Main {
 	public static void main(String[] args) {
 		Input input = new Input();
 		GameModel model = new GameModel();
+		//Test
 		GameController controller = new GameController(model, input);
 		Window window = new Window();
+		//Test
 		GamePanel panel = new GamePanel(model, controller);
 		input.setContainer(panel);
 		model.addListener(panel);
 		window.add(panel);
+		//Test
 		panel.requestFocus();
 	}
 }
