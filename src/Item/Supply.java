7cefa13b5e2f325811f9bb46388f27e6a4b35f9f
vidare <kompistillvidar@gmail.com>
2013-04-17 16:10:48 +0200

    Supply

---
+++ src/Item/Supply.java
@@ -0,0 +1,60 @@
+package Item;
+//TODO javadoc
+import javax.swing.Icon;
+
+/**
+ * 
+ * @author Vidar
+ *
+ */
+public class Supply {
+	
+	private int food;
+	private int ammo;
+	private int health;
+	private Icon icon;
+
+	/**
+	 * 
+	 * @param food
+	 * @param ammo
+	 * @param health
+	 * @param icon
+	 */
+	public Supply(int food, int ammo, int health, Icon icon){
+		this.food=food;
+		this.ammo=ammo;
+		this.health=health;
+		this.icon=icon;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getFood(){
+		return food;
+	}
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public int getAmmo(){
+		return ammo;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getHealth(){
+		return health;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public Icon getIcon(){
+		return icon;
+	}
+	
+}
