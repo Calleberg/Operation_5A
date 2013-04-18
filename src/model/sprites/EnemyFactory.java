5170298eff40813b9fbf62383379f07befe4f277
Jonatan Magnusson <kjb.magnusson@gmail.com>
2013-04-18 14:03:46 +0200

    small changes EnemyFactory
    Imported Weapon for future use
    Added position the method sign in EnemyFactory

--- src/model/sprites/EnemyFactory.java
+++ src/model/sprites/EnemyFactory.java
@@ -1,28 +1,29 @@
 package model.sprites;
 
 import model.geometrical.Position;
+import model.items.weapons.Weapon;
 
 public class EnemyFactory {
 	/**
 	 * Creates an easy enemy
 	 * @return an easy enemy
 	 */
-	public Enemy createEasyEnemy(){
-		return new Enemy(new Position(1,1), 0.1f, null, 50);
+	public Enemy createEasyEnemy(Position pos){
+		return new Enemy(pos, 0.1f, null, 50);
 	}
 	/**
 	 * Creates a medium enemy
 	 * @return a medium enemy
 	 */
-	public Enemy createMediumEnemy(){
-		return new Enemy(new Position(1,1), 0.2f, null, 100);
+	public Enemy createMediumEnemy(Position pos){
+		return new Enemy(pos, 0.2f, null, 100);
 	}
 	/**
 	 * Creates a hard enemy
 	 * @return a hard enemy
 	 */
-	public Enemy createHardEnemy(){
-		return new Enemy(new Position(1,1), 0.25f, null, 150);
+	public Enemy createHardEnemy(Position pos){
+		return new Enemy(pos, 0.25f, null, 150);
 	}
 
 }
