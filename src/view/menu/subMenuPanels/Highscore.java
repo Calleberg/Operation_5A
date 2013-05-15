1e7a4c8c6aee7e663a3cb7ffd3c519721dee70a8
vidare <kompistillvidar@gmail.com>
2013-05-15 15:14:06 +0200

    HighScoreModel

--- src/view/menu/subMenuPanels/Highscore.java
+++ src/view/menu/subMenuPanels/Highscore.java
@@ -26,11 +26,23 @@ public class Highscore extends SubMenuPanel {
 		JPanel p = new JPanel();
 		HighScoreModel h = new HighScoreModel();
 		
-		p.setLayout(new GridLayout(11, 0, 0, 10));
+		p.setLayout(new GridLayout(0, 1, 0, resources.Menu.getGap()));
 
 		for (int a=0; a<10 && a<h.getLenght(); a++){
-			JLabel l = new JLabel(
-					a+1 + ". " +h.getScore(a) + "p. " + h.getName(a));
+			String s = a+1 + ". "; 
+			if (h.getScore(a).length() < 1){
+				s+="No score ";
+			} else {
+				s+=h.getScore(a) + "p ";
+			}
+			
+			if (h.getName(a).length() < 1){
+				s+="No name";
+			} else {
+				s+=h.getName(a) + "";
+			}
+			
+			JLabel l = new JLabel(s);
 			l.setFont(resources.Menu.getLargeFont());
 			p.add(l);
 		}
