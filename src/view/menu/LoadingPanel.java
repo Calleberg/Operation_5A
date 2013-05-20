646290b7e695f9d9bf6fe0e5881e9917b77185b3
vidare <kompistillvidar@gmail.com>
2013-05-20 11:49:37 +0200

    Loadpanel

--- src/view/menu/LoadingPanel.java
+++ src/view/menu/LoadingPanel.java
@@ -5,17 +5,16 @@ import javax.swing.JLabel;
 
 import resources.MenuLookAndFeel;
 /**
+ * Shows a screen while loading other components.
  * @author Vidar Eriksson
  *
  */
 @SuppressWarnings("serial")
 public class LoadingPanel extends JPanel{
-	
 	public LoadingPanel() {
-		
 		JLabel lblLoading = new JLabel("Loading...");
 		add(lblLoading);
 		lblLoading.setFont(MenuLookAndFeel.getLargeFont());
+		setBackground(MenuLookAndFeel.getMenuColor());
 	}
-
 }
