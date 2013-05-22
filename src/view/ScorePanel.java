5da7385b722c8dfa067e3097b5bdb32907401605
Martin Calleberg <martin.calleberg@gmail.com>
2013-05-22 13:38:25 +0200

    Score fix

--- src/view/ScorePanel.java
+++ src/view/ScorePanel.java
@@ -3,6 +3,9 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Calendar;
 
 import javax.swing.JPanel;
 
@@ -16,10 +19,15 @@ import resources.Translator;
  * @author 
  *
  */
-public class ScorePanel extends JPanel {
+public class ScorePanel extends JPanel implements PropertyChangeListener {
 
 	private static final long serialVersionUID = 1L;
 	private GameModel model;
+	
+	private boolean timerOn = false;
+	private long timerStart;
+	private static final int TIME_MAX = 2000;
+	private int addedScore;
 	
 	/**
 	 * Creates a new instance of the object.
@@ -27,20 +35,42 @@ public class ScorePanel extends JPanel {
 	 */
 	public ScorePanel(GameModel model) {
 		super();
-		this.setPreferredSize(new Dimension(200, 45));
+		this.setPreferredSize(new Dimension(200, 90));
 		this.model = model;
-		
+		model.addListener(this);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		g.setColor(new Color(0, 0, 0, 100));
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.fillRect(0, 0, this.getWidth(), this.getHeight()/2);
 		
 		g.setFont(HUDFonts.getScoreFont());
 		g.setColor(Color.WHITE);
 		String text = Translator.getMenuString("score") + ": " + model.getScore();
 		g.drawString(text, this.getWidth()/2 - 
 				(int)g.getFontMetrics().getStringBounds(text, g).getWidth()/2, 30);
+		
+		// Display just added score
+		if(timerOn) {
+			if(Calendar.getInstance().getTimeInMillis() - timerStart > TIME_MAX) {
+				timerOn = false;
+			}
+			text = "+" + addedScore;
+			g.setColor(Color.GREEN);
+			g.drawString(text, this.getWidth()/2 - 
+					(int)g.getFontMetrics().getStringBounds(text, g).getWidth()/2, 60);
+		}		
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		if(evt.getPropertyName().equals(GameModel.EVENT_ADDED_SCORE)) {
+			if((Integer)evt.getNewValue() > 1) {
+				addedScore = (Integer)evt.getNewValue();			
+				timerOn = true;
+				timerStart = Calendar.getInstance().getTimeInMillis();
+			}
+		}
 	}
 }
